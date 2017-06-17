af5d9dbe05afbc4daa5c8f3f1fce8dd056bb3b26
Paul Barnabas <bizpally@gmail.com>
2017-06-17 13:31:12 +0100

    Setup bootstrap framework as page display design for both online and offline use

--- index.php
+++ index.php
@@ -5,3 +5,79 @@
  * Date: 6/17/2017
  * Time: 12:25 PM
  */
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="First PHP Website and Blog" content="Page Description">
+    <meta name="Paul Barnabas" content="Made with love by Paul B">
+    <title>PAUL BARNABAS</title>
+
+    <!-- Bootstrap -->
+    <!--    <link rel="stylesheet" href="https://bootswatch.com/simplex/bootstrap.min.css">-->
+    <link href="asset/css/bootstrap.min.css" rel="stylesheet">
+    <link href="asset/css/styles.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <!--This is the header Section-->
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">PAUL BARNABAS</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Add Post</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <div class="container text-center">
+            <h1>Hello, Welcome to my Website</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+    </div>
+
+
+    <!-- This is the body of the homepage-->
+    <div class="container">
+        <h1>Body</h1>
+
+    </div>
+
+    <!-- This is the footer Section-->
+    <footer>
+        <p class="text-center">&copy; 2017 <a href="https://www.twitter.com/barnypally" target="_blank">Paul Barnabas</a></p>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
+    <script src="asset/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+    <script src="asset/js/bootstrap.min.js"></script>
+</body>
+</html>
+
