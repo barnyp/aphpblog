db4e8f2f4e79939bb8c59bc43ca42bc8aa08f54a
Paul Barnabas <bizpally@gmail.com>
2017-06-17 12:34:04 +0100

    Initial Commit with basic file structure

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ITSUPPORT-05
+ * Date: 6/17/2017
+ * Time: 12:31 PM
+ */
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by @barnypally.
+ * User: Paul Barnabas
+ * Date: 6/17/2017
+ * Time: 12:25 PM
+ */
